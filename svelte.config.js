9f87e23c9f054995d89204c4b151dd80e4180fa5
Dmitry Statsenko <alkor.sx@gmail.com>
2024-01-22 17:39:46 +0300

    refactor config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,28 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import preprocess from 'svelte-preprocess'
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [
+    vitePreprocess(),
+    preprocess({
+      babel: {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              loose: true,
+              // No need for babel to resolve modules
+              modules: false,
+              targets: {
+                // ! Very important. Target es6+
+                esmodules: true,
+              },
+            },
+          ],
+        ],
+      },
+    })
+  ],
 }
